6d628a09f02d00b456dfbedf10ad1227397ac1cf
Lidya Tsegay <lidyatsegay@outlook.com>
2019-10-23 14:30:49 -0400

    vendor-detail completed

--- src/app/prs/vendor/vendor-detail/vendor-detail.component.ts
+++ src/app/prs/vendor/vendor-detail/vendor-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { VendorService} from '../../vendor/vendor.service';
+import { Vendor } from '../vendor.class';
 
 @Component({
   selector: 'app-vendor-detail',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorDetailComponent implements OnInit {
 
-  constructor() { }
+  vendor: Vendor;
+  verifyDelete: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private vendorsvc: VendorService
+  ) { }
+
+  edit(): void{
+    this.router.navigateByUrl(`/vendor/edit/${this.vendor.id}`)
+  }
+
+  verify(): void {
+    this.verifyDelete = !this.verifyDelete;
+  }
+  
+  delete(): void {
+    this.vendorsvc.remove(this.vendor).subscribe(
+      res => {
+        console.log("This vendor removed", res);
+        this.router.navigateByUrl(`vendor/list`);
+      },
+      err =>{
+        console.error(err);
+      }
+    )
+  }
 
   ngOnInit() {
+    let vendorid = this.route.snapshot.params.id;
+    this.vendorsvc.get(vendorid).subscribe(
+      vendor => {
+        this.vendor = vendor;
+        console.log("Vendor:", vendor);
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
 }
