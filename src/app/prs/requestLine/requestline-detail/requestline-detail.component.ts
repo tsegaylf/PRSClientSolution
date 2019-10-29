3a02944980a670a8f64cb0cff0aab6e9de3fc25f
Lidya Tsegay <lidyatsegay@outlook.com>
2019-10-29 11:32:42 -0400

    added hidden to all id fields

--- src/app/prs/requestLine/requestline-detail/requestline-detail.component.ts
+++ src/app/prs/requestLine/requestline-detail/requestline-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { RequestlineService } from '../requestline.service';
+import { Requestline } from '../requestline.class';
 
 @Component({
   selector: 'app-requestline-detail',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestlineDetailComponent implements OnInit {
 
-  constructor() { }
+  requestline: Requestline;
+  verifyDelete: boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private requestlinesvc: RequestlineService
+  ){ }
+
+  edit(): void {
+    this.router.navigateByUrl(`/requestlines/edit/${this.requestline.id}`);
+  }
+
+  verify(): void {
+   this.verifyDelete = !this.verifyDelete;
+  }
+
+  delete(): void {
+    this.requestlinesvc.remove(this.requestline).subscribe(
+      res => {console.log("Product change resp:", res); 
+    this.router.navigateByUrl('/requestlines/list');
+    }, 
+    err => {console.log(err);}
+    );
+  }
 
   ngOnInit() {
+    let requestlineid = this.route.snapshot.params.id;
+    this.requestlinesvc.get(requestlineid).subscribe(
+    requestline => {
+      this.requestline =requestline;
+      console.log("Requestline:", requestline);
+    },
+    err => {
+      console.error(err);
+    });
   }
 
 }
